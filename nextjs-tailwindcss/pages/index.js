df422689f13117e2d2a0d49dfc4f90e631d0c0be
chaching1 <ijoroyo748@gmail.com>
2021-01-08 14:23:17 +0700

    Edited readme.md (#3)
    * Next.Js
    
    Simple Next.Js boilerplate.
    
    * More boilerplate published
    
    * Edited readme.md

---
+++ nextjs-tailwindcss/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div className="flex items-center justify-center bg-red-500 text-white font-bold py-48">Hello world!</div>
+      </main>
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
---
+++ nextjs-tailwindcss/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.js', './components/**/*.js'], // Purging unused styles
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
